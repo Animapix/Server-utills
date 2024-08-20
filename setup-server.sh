65eb2992a2efa1209a3082c9582a5bd6c0d159a3
POINTET Nicolas <nicolas@animapix.fr>
2024-08-20 23:20:20 +0200

    chore: Refactor setup-server.sh script to improve readability and add French translation

--- setup-server.sh
+++ setup-server.sh
@@ -5,41 +5,50 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-#_______________________________________________NEW USER______________________________________________________
+#_______________________________________________NEW USER__________________________________________________________
 
-read -p "Entrez le nom du nouvel utilisateur: " NEW_USER
+read -p "Voulez-vous créer un utilisateur ? (oui/non) : " REPLY
 
-#adduser $NEW_USER
-#usermod -aG sudo $NEW_USER
-#mkdir -p /home/$NEW_USER/.ssh
-#chmod 700 /home/$NEW_USER/.ssh
-#systemctl restart ssh
-echo "Le nouvel utilisateur $NEW_USER a été créé avec succès et ajouté au groupe sudo."
+if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
+    read -p "Entrez le nom du nouvel utilisateur: " NEW_USER
+    adduser $NEW_USER
+    usermod -aG sudo $NEW_USER
+    mkdir -p /home/$NEW_USER/.ssh
+    chmod 700 /home/$NEW_USER/.ssh
+    systemctl restart ssh
+    echo "Le nouvel utilisateur $NEW_USER a été créé avec succès et ajouté au groupe sudo."
+else
 
 #_______________________________________________DISABLE USER______________________________________________________
-
-disable_user() {
-  local USER_TO_DISABLE=$1
-  #usermod -L $USER_TO_DISABLE
-  #echo "DenyUsers $USER_TO_DISABLE" >> /etc/ssh/sshd_config
-  echo "L'utilisateur $USER_TO_DISABLE a été verrouillé et son accès SSH a été désactivé."
-}
 
 while true; do
   read -p "Voulez-vous désactiver un utilisateur ? (oui/non) : " REPLY
   if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
     read -p "Entrez le nom de l'utilisateur à désactiver: " USER_TO_DISABLE
-    disable_user $USER_TO_DISABLE
+    usermod -L $USER_TO_DISABLE
+    echo "DenyUsers $USER_TO_DISABLE" >> /etc/ssh/sshd_config
+    echo "L'utilisateur $USER_TO_DISABLE a été verrouillé et son accès SSH a été désactivé."
   else
     break
   fi
 done
 
-read -p "Entrez le nom du serveur: " NEW_HOSTNAME
+#_______________________________________________CHANGE SERVER NAME_______________________________________________
 
-#sudo hostnamectl set-hostname $NEW_HOSTNAME
-#echo $NEW_HOSTNAME | sudo tee /etc/hostname
-#sudo sed -i "s/127.0.1.1.*/127.0.1.1    $NEW_HOSTNAME/" /etc/hosts
+read -p "Voulez-vous changer le nom de votre serveur ? (oui/non) : " REPLY
 
-echo "Le nom d'hôte a été changé en $NEW_HOSTNAME. Le serveur va maintenant redémarrer."
-#sudo reboot
+if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
+    read -p "Entrez le nom du serveur: " NEW_HOSTNAME
+    sudo hostnamectl set-hostname $NEW_HOSTNAME
+    echo $NEW_HOSTNAME | sudo tee /etc/hostname
+    sudo sed -i "s/127.0.1.1.*/127.0.1.1    $NEW_HOSTNAME/" /etc/hosts
+    echo "Le nom d'hôte a été changé en $NEW_HOSTNAME."
+else
+#_______________________________________________REBOOT SERVER_____________________________________________________
+
+read -p "Voulez-vous redémarer le serveur ? (oui/non) : " REPLY
+
+if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
+    echo "Le serveur va maintenant redémarrer."
+    sudo reboot
+else
