9bf6cb2faf4bfda7ea18c1934d41191f73596a0d
POINTET Nicolas <nicolas@animapix.fr>
2024-08-20 23:44:38 +0200

    chore: Refactor setup-server.sh script to improve readability, add French translation, and update user prompts

--- setup-server.sh
+++ setup-server.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Ce script doit être exécuté avec des privilèges root. Utilisez sudo."
+  echo "This script must be run as root. Please use sudo."
   exit 1
 fi
 
 #_______________________________________________NEW USER__________________________________________________________
 
-read -p "Voulez-vous créer un utilisateur ? (oui/non) : " REPLY
+read -p "Do you want to create a new user? (yes/no) : " REPLY
 
-if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
-    read -p "Entrez le nom du nouvel utilisateur: " NEW_USER
+if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
+    read -p "Enter the name of the new user: " NEW_USER
     adduser $NEW_USER
     usermod -aG sudo $NEW_USER
     mkdir -p /home/$NEW_USER/.ssh
     chmod 700 /home/$NEW_USER/.ssh
     systemctl restart ssh
-    echo "Le nouvel utilisateur $NEW_USER a été créé avec succès et ajouté au groupe sudo."
+    echo "The new user $NEW_USER has been successfully created and added to the sudo group."
 fi
 
 #_______________________________________________DISABLE USER______________________________________________________
 
 while true; do
-  read -p "Voulez-vous désactiver un utilisateur ? (oui/non) : " REPLY
-  if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
-    read -p "Entrez le nom de l'utilisateur à désactiver: " USER_TO_DISABLE
+  read -p "Do you want to disable a user? (yes/no) : " REPLY
+  if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
+    read -p "Enter the name of the user to disable: " USER_TO_DISABLE
     usermod -L $USER_TO_DISABLE
     echo "DenyUsers $USER_TO_DISABLE" >> /etc/ssh/sshd_config
-    echo "L'utilisateur $USER_TO_DISABLE a été verrouillé et son accès SSH a été désactivé."
+    echo "The user $USER_TO_DISABLE has been locked and their SSH access disabled."
   else
     break
   fi
@@ -35,21 +35,21 @@ done
 
 #_______________________________________________CHANGE SERVER NAME_______________________________________________
 
-read -p "Voulez-vous changer le nom de votre serveur ? (oui/non) : " REPLY
+read -p "Do you want to change the server name? (yes/no) : " REPLY
 
-if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
-    read -p "Entrez le nom du serveur: " NEW_HOSTNAME
+if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
+    read -p "Enter the new server name: " NEW_HOSTNAME
     sudo hostnamectl set-hostname $NEW_HOSTNAME
     echo $NEW_HOSTNAME | sudo tee /etc/hostname > /dev/null
     sudo sed -i "s/127.0.1.1\s\+.*/127.0.1.1    $NEW_HOSTNAME/" /etc/hosts
-    echo "Le nom d'hôte a été changé en $NEW_HOSTNAME."
+    echo "The hostname has been changed to $NEW_HOSTNAME."
 fi
 
 #_______________________________________________REBOOT SERVER_____________________________________________________
 
-read -p "Voulez-vous redémarrer le serveur ? (oui/non) : " REPLY
+read -p "Do you want to reboot the server? (yes/no) : " REPLY
 
-if [[ "$REPLY" =~ ^[Oo][Uu][Ii]$ ]]; then
-    echo "Le serveur va maintenant redémarrer."
+if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
+    echo "The server will now reboot."
     sudo reboot
 fi
